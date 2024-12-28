067f4f9e771bc7e179654c901e6a8b6fd767946a
BlueyHeeler <tairaerick@gmail.com>
2024-12-28 15:31:59 -0300

    Organizando o código com o cpplint.

--- testa_conta_palavras.cpp
+++ testa_conta_palavras.cpp
@@ -9,6 +9,7 @@
 TEST_CASE("Testa conta_palavras", "[single-file]") {
     REQUIRE(abrirTXT("input.txt") == "Este texto é o texto que será utilizado");
 
-    std::vector<std::string> esperado = {"Este","texto","é","o","texto","que","será","utilizado"};
+    std::vector<std::string> esperado = {"Este", "texto", "é", "o",
+                                         "texto", "que", "será", "utilizado"};
     REQUIRE(iteraTXT(abrirTXT("input.txt")) == esperado);
 }
